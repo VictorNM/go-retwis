464c5eb3f022fea2c4c2d02642a762119d9ba181
victornm <victor.nguyenmau@gmail.com>
2020-05-15 19:54:36 +0700

    Read from env

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/go-playground/validator/v10"
 	"github.com/go-redis/redis/v7"
 	"github.com/labstack/echo/v4"
+	"os"
+	"strconv"
 )
 
 type myValidator struct {
@@ -17,20 +19,18 @@ func (v *myValidator) Validate(i interface{}) error {
 
 func main() {
 	var (
-		httpAddr string
-
-		redisAddr string
-		redisPass string
-		redisDB   int
-
-		jwtSecret string
+		httpAddr  = strEnv("HTTP_ADDR", ":8080")
+		redisAddr = strEnv("REDIS_ADDR", ":6379")
+		redisPass = strEnv("REDIS_PASS", "")
+		redisDB   = intEnv("REDIS_DB", 0)
+		jwtSecret = strEnv("JWT_SECRET", "")
 	)
 
-	flag.StringVar(&httpAddr, "http-addr", ":8080", "address for serving HTTP")
-	flag.StringVar(&redisAddr, "redis-addr", ":6379", "address for redis")
-	flag.StringVar(&redisPass, "redis-pass", "", "password for redis")
-	flag.IntVar(&redisDB, "redis-db", 0, "redis database index")
-	flag.StringVar(&jwtSecret, "jwt-secret", "", "secret key for jwt")
+	httpAddr 	= *flag.String("http-addr", httpAddr, "address for serving HTTP")
+	redisAddr 	= *flag.String("redis-addr", redisAddr, "address for redis")
+	redisPass	= *flag.String("redis-pass", "", "password for redis")
+	redisDB		= *flag.Int("redis-db", 0, "redis database index")
+	jwtSecret 	= *flag.String("jwt-secret", jwtSecret, "secret key for jwt")
 
 	flag.Parse()
 
@@ -48,4 +48,20 @@ func main() {
 	s.routes()
 
 	e.Logger.Fatal(s.e.Start(httpAddr))
+}
+
+func intEnv(name string, value int) int {
+	if v, e := strconv.Atoi(os.Getenv(name)); e == nil {
+		return v
+	}
+
+	return value
+}
+
+func strEnv(name string, value string) string {
+	if v := os.Getenv(name); v != "" {
+		return v
+	}
+
+	return value
 }
